aa54e149aee8b6a03762cf07accc013dcd9bd5af
npgiano-macbook <npgiano@gmail.com>
2022-09-08 10:08:18 -0400

    moved to dirs

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 echo "Cleaning existing classes..."
-rm -f *.class
+# rm -f ./src/main/java/*.class # no point in this existing if the below command does the same work
 # This command looks for matching files and runs the rm command for each file
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/main/java/*.java ./src/test/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build org.junit.runner.JUnitCore EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java RunEdgeConvert
+java -cp build RunEdgeConvert
